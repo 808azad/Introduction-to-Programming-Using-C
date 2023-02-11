7c5dbedabace410b11e2e27fc99ade0e589bb514
808azad <azeynalov1@myseneca.ca>
2023-02-11 12:50:20 -0500

    Utils!

---
+++ workshops/ws4/lab_week5/week5_lab/utils.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+int getInt();
+void flushKey();
+
+
+
+
+int getInt(void) {
+    int value;
+    int matches = scanf("%d", &value);
+    char c = getchar();
+    while (matches != 1 || c != '\n') {
+        flushKey();
+        if (matches != 1) {
+            printf("Invalid Integer, try again: ");
+        }
+        else {
+            printf("Enter only an integer, try again: ");
+        }
+        matches = scanf("%d", &value);
+        c = getchar();
+    }
+    return value;
+}
+
+void flushKey() {
+    char ch = 'x';
+    while (ch != '\n') {
+        ch = getchar();
+    }
+}
+
+
+
