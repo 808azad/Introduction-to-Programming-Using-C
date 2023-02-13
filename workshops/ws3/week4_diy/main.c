b4349af6c69879c17cb21683a4bdd908effa2bb1
Azad Zeynalov <122503717+808azad@users.noreply.github.com>
2023-02-12 19:36:16 -0500

    Update main.c

--- workshops/ws3/week4_diy/main.c
+++ workshops/ws3/week4_diy/main.c
@@ -10,22 +10,22 @@ void line(char fill, int len);
 int main(void) {
    int value;
    double dvalue;
-   printf("\rEnter an integer value: ");
+   printf("Enter an integer value: ");
    value = getInt();
    printf("You entered: %d\n", value);
    line('*', 50);
    //********************************************
-   printf("\rEnter an integer value between 10 and 20 inclusive: ");
+   printf("Enter an integer value between 10 and 20 inclusive: ");
    value = getIntMM(10, 20);
    printf("You entered: %d\n", value);
    line('*', 50);
    //********************************************
-   printf("\rEnter a double value: ");
+   printf("Enter a double value: ");
    dvalue = getDouble();
    printf("You entered: %.2lf\n", dvalue);
    line('*', 50);
    //********************************************
-   printf("\rEnter a double value between 10.1 and 20.9 inclusive: ");
+   printf("Enter a double value between 10.1 and 20.9 inclusive: ");
    dvalue = getDoubleMM(10.1, 20.9);
    printf("You entered: %.2lf\n", dvalue);
    //********************************************
