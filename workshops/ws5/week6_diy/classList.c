9287ca5df4a303410091c40188d63404263b5bbd
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2023-03-03 13:55:42 -0500

    Fardad's suggestions

--- workshops/ws5/week6_diy/classList.c
+++ workshops/ws5/week6_diy/classList.c
@@ -5,90 +5,83 @@
 
 
 
-
-
-
-void printReport(const char subjectCode[], const int row[], const int studentNumbers[],
-    const int marks[], int noOfStudents) {
-
-
-    int i, average, totalMarks = 0, lowestMark = 100, highestMark = 0;
-
-
-
-    char c = ' ';
-    printf("%cSUBJECT MARKS REPORT!\n", c);
-    printf("%c+-------------------+\n",c);
-    printf("%c|%10s%9c|\n", c, subjectCode, c);
-    printf("%c+---+--------+------+\n",c);
-    printf("%c|Row| Std No | mark |\n", c);
-    printf("%c+---+--------+------+\n", c);
-
-
-    for (i = 0; i < noOfStudents; i++) {
-
-        printf("%c| %d | %06d |%5d |\n", c, row[i], studentNumbers[i], marks[i]);
-      
-
-        totalMarks += marks[i];
-
-        if (marks[i] < lowestMark) {
-            lowestMark = marks[i];
-        }
-        if (marks[i] > highestMark) {
-            highestMark = marks[i];
-
-        }
-
-    }
-    average = totalMarks / noOfStudents;
-
-
-    printf("%c+---+--------+------+\n", c);
-    printf("%c|%cAverage %3c|%5d |\n", c, c, c, average);
-    printf("%c|%cHighest %3c|%5d |\n", c, c, c, highestMark);
-    printf("%c|%cLowest  %3c|%5d |\n", c, c, c, lowestMark);
-    printf("%c+------------+------+\n", c);
-
-}
-
 void subjectMarksReport(void) {
-
-    int i;
     int numberOfStudents;
-
     int marks[MAX];
     int studentsNum[MAX];
     int row[MAX];
-
     char subjCode[10];
-
-
-    printf("Enter subject Name: ");
-    scanf("%s", subjCode);
-
-    printf("Enter Number of students (max 50): ");
-    numberOfStudents = getMMInt(1, 50, "Number of students");
-
-
-    printf("Please enter %d student marks:\n", numberOfStudents);
-    
-
-
-    for (i = 0; i < numberOfStudents; i++) {
-        row[i] = i + 1;
-        printf("%d of %d:\n", i + 1, numberOfStudents);
-        printf("Student Number: ");
-        studentsNum[i] = getMMInt(9999, 1000000, "Student Number");
-        printf("Mark: ");
-        marks[i] = getMMInt(0, 100, "Mark");
-
-
-    }
-    printf("\n");
+    numberOfStudents = getSubjectInfo(subjCode);
+    getStudentMarks(numberOfStudents, row, studentsNum, marks);
     printReport(subjCode, row, studentsNum, marks, numberOfStudents);
-   
-    
-   
 }
 
+
+
+
+void getStudentMarks(int numberOfStudents, int row[], int studentsNum[], int marks[]) {
+   int i;
+   printf("Please enter %d student marks:\n", numberOfStudents);
+   for(i = 0; i < numberOfStudents; i++) {
+      row[i] = i + 1;
+      printf("%d of %d:\n", i + 1, numberOfStudents);
+      printf("Student Number: ");
+      studentsNum[i] = getMMInt(9999, 1000000, "Student Number");
+      printf("Mark: ");
+      marks[i] = getMMInt(0, 100, "Mark");
+   }
+   printf("\n");
+
+}
+void printReport(const char subjectCode[], const int row[], const int studentNumbers[],
+                 const int marks[], int noOfStudents) {
+   int
+      i,
+      average,
+      totalMarks = 0,
+      lowestMark = 100,
+      highestMark = 0;
+
+   printf(" SUBJECT MARKS REPORT!\n");
+   printf(" +-------------------+\n");
+   printf(" |%19s|\n", subjectCode);
+   printf(" +---+--------+------+\n");
+   printf(" |Row| Std No | mark |\n");
+   printf(" +---+--------+------+\n");
+
+
+   for(i = 0; i < noOfStudents; i++) {
+
+      printf(" | %d | %06d |%5d |\n", row[i], studentNumbers[i], marks[i]);
+
+
+      totalMarks += marks[i];
+
+      // you can get the total marks here
+      if(marks[i] < lowestMark) {
+         lowestMark = marks[i];
+      }
+      if(marks[i] > highestMark) {
+         highestMark = marks[i];
+
+      }
+
+   }
+   average = totalMarks / noOfStudents;
+
+
+   printf("%c+---+--------+------+\n", c);
+   printf("%c|%cAverage %3c|%5d |\n", c, c, c, average);
+   printf("%c|%cHighest %3c|%5d |\n", c, c, c, highestMark);
+   printf("%c|%cLowest  %3c|%5d |\n", c, c, c, lowestMark);
+   printf("%c+------------+------+\n", c);
+
+}
+int getSubjectInfo(char subjCode[]) {
+   int numberOfStudents;
+   printf("Enter subject Name: ");
+   scanf("%s", subjCode);
+   printf("Enter Number of students (max 50): ");
+   numberOfStudents = getMMInt(1, 50, "Number of students");
+   return numberOfStudents;
+}
--- workshops/ws5/week6_diy/classList.h
+++ workshops/ws5/week6_diy/classList.h
@@ -1,3 +1,23 @@
-void printReport(const char subjectCode[], const int studentNumbers[],
-    const int marks[], int noOfStudents);
+
+void printReport(const char subjectCode[], const int row[], const int studentNumbers[],
+                 const int marks[], int noOfStudents);
+
 void subjectMarksReport(void);
+
+/// <summary>
+/// Gets the subject information from console (Number of students and subject code)
+/// </summary>
+/// <param name="subjCode">Passes back the subject code read from console</param>
+/// <returns>Number of students read from console</returns>
+int getSubjectInfo(char subjCode[]);
+
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="numberOfStudents"></param>
+/// <param name="row"></param>
+/// <param name="studentsNum"></param>
+/// <param name="marks"></param>
+void getStudentMarks(int numberOfStudents, int row[], int studentsNum[], int marks[]);
+
